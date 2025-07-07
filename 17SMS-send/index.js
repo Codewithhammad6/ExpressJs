e3ba34490eaa864aae32a05863c4007f31b1d16b
Hammad Chaudhary <hammadp5087@gmail.com>
2025-07-07 08:45:38 +0500

    sms

---
+++ 17SMS-send/index.js
@@ -0,0 +1,81 @@
+//                    When we send SMS ?
+// Login Alert
+// Send OTP for login
+// Payment Confirmation
+// Order Confirmation
+// Shipping & Delivery Updates
+// Appointment Reminders
+// Subscription Renewals
+// Event Reminders
+// Promotional Offers
+// Welcome Messages
+
+
+
+//// npm install twilio
+// const twilio = require('twilio');
+
+// const accountSid = 1122334455;
+// const authToken = j5k34k3kcvk8v7c7;
+// const client = new twilio (accountSid, authToken);
+
+// client.messages.create({
+// body: 'Hello from Hammad',
+// from: +9112345678,
+// to: +91987654321
+// })
+
+
+
+
+
+
+
+
+
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+const twilio = require('twilio')
+
+dotenv.config();
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+
+const accountSid=process.env.TWILIO_ACCOUNT_SID
+const authToken=process.env.TWILIO_AUTH_TOKEN
+const client = new twilio(accountSid,authToken)
+
+
+app.post('/send-sms',async (req, res) => {
+const {to, message} = req.body
+try {
+    const result =await client.call.create({
+    body:message,
+    from:process.env.TWILIO_PHONE_NUMBER,
+    to:to
+})
+res.status(200).json({
+    sid:result.sid,    //twilio sid return krta ha usay hum dakh saktay hain asay
+   message:'SMS send successfully.'
+})
+} catch (error) {
+    res.status(500).json({
+        message:"Failed to send SMS.",
+        error:error.message
+    })
+}
+});
+
+
+app.get('/', (req, res) => {
+res.render('smspage');
+});
+
+app.listen(4000, () => {
+console.log(`Successfully Connectes http://localhost:${4000}`)
+});
